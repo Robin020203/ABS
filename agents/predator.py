7db769aaae577fe59e23bdf29a846ce10cb9781c
Laurens ten Wolde <laurenstenwolde@Laurens-MacBook-Bro-4.local>
2025-05-11 15:37:11 +0200

    vision inheritance

--- agents/predator.py
+++ agents/predator.py
@@ -7,11 +7,14 @@ class Predator(Animal):
     def __init__(self,
                  world,
                  position: Optional[Tuple[float, float]] = None,
-                 vision_range: Optional[int] = None,
-                 vision_width: Optional[int] = None):
+                 vision_range: float = max(17.0, min(23.0, random.gauss(20, 1))),
+                 vision_width: Optional[float] = None):
         super().__init__(world, position)
-        self.vision_range = 0 #yet to be implemented
-        self.vision_width = 0 #yet to be implemented
+        if not vision_width:
+            vision_width = 20 - (3.33 * (20 - vision_range))
+        vision_mutation = random.gauss(0, 0.5)
+        self.vision_range = max(17.0, min(23.0, vision_range + vision_mutation))
+        self.vision_width = max(10.0, min(30.0, vision_width - (3.33 * vision_mutation)))
         self.energy_consumption = 2 # how much energy spent each timestep when moving normally
         self.hunger = 0 # timesteps since last meal
         self.max_hunger = 50 # timesteps before dying of hunger
--- agents/prey.py
+++ agents/prey.py
@@ -6,11 +6,14 @@ class Prey(Animal):
     def __init__(self,
                  world,
                  position: Optional[Tuple[float, float]] = None,
-                 vision_range: Optional[int] = None,
-                 vision_width: Optional[int] = None):
+                 vision_range: float = max(7.0, min(13.0, random.gauss(10, 1))),
+                 vision_width: Optional[float] = None):
         super().__init__(world, position)
-        self.vision_range = 0 #TODO
-        self.vision_width = 0 #TODO
+        if not vision_width:
+            vision_width = 60 - (6.66 * (10 - vision_range))
+        vision_mutation = random.gauss(0, 1)
+        self.vision_range = max(7.0, min(13.0, vision_range + vision_mutation))
+        self.vision_width = max(40.0, min(80.0, vision_width - (6.66 * vision_mutation)))
         self.energy_consumption = 1
         self.time_alive = 0
         self.reproduction_threshold = 50 # after this amount of timesteps, the prey reproduces
